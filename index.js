9a54a2cb25428fb2e44fe24adcdea3be3a34469e
Josue V. Alorro <joalorro@gmail.com>
2018-06-12 15:00:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
+=======
+var greetSpecialGuest = "Hello" + specialGuest + "!";
+>>>>>>> 9a4a11b7502c1a8bdd67107b494e6ea23e4b7933
 
 var topic = "space";
 
